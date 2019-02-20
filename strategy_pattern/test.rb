21230a4593c69348ab46d57084ab8eff61378d23
YourSouLi5Mine <precichysim@gmail.com>
2019-02-19 22:57:15 -0600

    delete orphan autoload

--- strategy_pattern/test.rb
+++ strategy_pattern/test.rb
@@ -3,7 +3,6 @@ require 'active_support'
 autoload :Report, './strategy_pattern/report'
 autoload :HTMLFormatter, './strategy_pattern/html_formatter'
 autoload :PlainTextFormatter, './strategy_pattern/plain_text_formatter'
-autoload :Formatter, './strategy_pattern/formatter'
 
 report = Report.new(HTMLFormatter.new)
 report.output_report
